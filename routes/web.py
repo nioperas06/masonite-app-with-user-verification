a98d40ef9b2196b32cacb88b2df298689724afa5
Junior Gantin <nioperas06@gmail.com>
2018-09-12 14:29:23 +0100

    Update web.py

--- routes/web.py
+++ routes/web.py
@@ -1,17 +1,16 @@
 ''' Web Routes '''
-from masonite.routes import Get, Post
-from masonite.helpers.routes import get
+from masonite.helpers.routes import get, post
 
 ROUTES = [
     Get().route('/', 'WelcomeController@show').name('welcome'),
 ]
 
 ROUTES = ROUTES + [
-    Get().route('/login', 'LoginController@show'),
-    Get().route('/logout', 'LoginController@logout'),
-    Post().route('/login', 'LoginController@store'),
-    Get().route('/register', 'RegisterController@show'),
-    Post().route('/register', 'RegisterController@store'),
-    Get().route('/home', 'HomeController@show'),
+    get('/login', 'LoginController@show'),
+    get('/logout', 'LoginController@logout'),
+    post('/login', 'LoginController@store'),
+    get('/register', 'RegisterController@show'),
+    post('/register', 'RegisterController@store'),
+    get('/home', 'HomeController@show'),
     get('/activate/@token', 'RegisterController@validate')
 ]
